8e090e4465534ccb385f9498890d88f269759102
晚风拂柳颜 <434857005@qq.com>
2024-04-03 16:55:19 +0800

    1

--- test_module.py
+++ test_module.py
@@ -4,20 +4,32 @@
 # Author: DaShenHan&道长-----先苦后甜，任凭晚风拂柳颜------
 # Author's Blog: https://blog.csdn.net/qq_32394351
 # Date  : 2024/3/31
+import os
+import sys
 
 from pyquickjs import Context
 
 if __name__ == '__main__':
     module_js = """
-    function add(a,b){
+    function addd(a,b){
     return a+b
     }
-    globalThis.add = add;
-    module.exports = {
-    add
+    export default {
+    addd
     }
     """
+    mod_test = '''import { fib } from "./fib_module.js";
+    import q from "./1.js";
+    import "./crypto-js.js";
+    console.log("Hello World 你好");
+
+    console.log(typeof(q.add));
+
+    console.log("fib(10)=", fib(10));'''
     ctx = Context()
-    ctx.module(module_js)
-    ret = ctx.eval('add(1,2)')
-    print('ret:', ret)
+    print("work dir:", os.getcwd())
+    ctx.eval('let a=1+3')
+
+    ctx.module(module_js, "11.js")
+    ctx.module(mod_test,"")
+    ctx.module('''import qq from "11.js";\nconsole.log(typeof(qq.addd));''',"2.js")
